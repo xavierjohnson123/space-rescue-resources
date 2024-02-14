74cfc232adf68b14ba6a1274c45a7c6be99b6b37
xavierjohnson123 <158520882+xavierjohnson123@users.noreply.github.com>
2024-02-14 13:40:06 +1000

    kept ship on screen

--- Objects/Ship.py
+++ Objects/Ship.py
@@ -1,4 +1,4 @@
-from GameFrame import RoomObject
+from GameFrame import RoomObject, Globals
 import pygame
 
 class Ship(RoomObject):
@@ -27,3 +27,18 @@ class Ship(RoomObject):
             self.y_speed = -10
         elif key[pygame.K_s]:
             self.y_speed = 10
+
+    def keep_in_room(self):
+        """
+        Keeps the ship inside the room
+        """
+        if self.y < 0:
+            self.y = 0
+        elif self.y + self.height> Globals.SCREEN_HEIGHT:
+            self.y = Globals.SCREEN_HEIGHT - self.height
+
+    def step(self):
+        """
+        Determine what happens to the Ship on each click of the game clock
+        """
+        self.keep_in_room()
