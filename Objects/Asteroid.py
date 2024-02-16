44a3cda2e5885a7ee9f6eb03c810b25da9493aec
xavierjohnson123 <158520882+xavierjohnson123@users.noreply.github.com>
2024-02-16 14:41:42 +1000

    move asteroid

--- Objects/Asteroid.py
+++ Objects/Asteroid.py
@@ -1,4 +1,5 @@
-from GameFrame import RoomObject
+from GameFrame import RoomObject, Globals
+import random
 
 class Asteroid(RoomObject):
     """
@@ -10,8 +11,49 @@ class Asteroid(RoomObject):
         Initialise the Asteroid object
         """
         # include attributes and methods from RoomObject
-        RoomObject.__init__(self, room, x, y)
-
+        RoomObject.__init__(self,room, x, y)
+        
         # set image
         image = self.load_image("asteroid.png")
         self.set_image(image,50,49)
+        
+        # set travel direction
+        angle = random.randint(135,225)
+        self.set_direction(angle, 10)
+
+        # register events
+        self.register_collision_object("Ship")
+        
+    def step(self):
+        """
+        Determines what happens to the asteroid on each tick of the game clock
+        """
+        self.keep_in_room()
+        self.outside_of_room()
+        
+    def keep_in_room(self):
+        """
+        Keeps the asteroid inside the top and bottom room limits
+        """
+        if self.y < 0:
+            self.y = 0
+            self.y_speed *= -1
+        elif self.y > Globals.SCREEN_HEIGHT - self.height:
+            self.y = Globals.SCREEN_HEIGHT - self.height
+            self.y_speed *= -1
+            
+    def outside_of_room(self):
+        """
+        removes asteroid that have exited the room
+        """
+        if self.x + self.width < 0:
+            print("asteroid deleted")
+            self.room.delete_object(self)
+
+    def handle_collision(self, other, other_type):
+        """
+        Handles the collision events for the Asteroid
+        """
+        
+        if other_type == "Ship":
+            self.room.running = False
